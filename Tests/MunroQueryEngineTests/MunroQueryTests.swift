f00e1965ee392ce539f832276c2f8ebf924e75b1
Olivier Butler <olivier.butler@gmail.com>
2021-03-24 18:19:16 +0000

    Wrote code for query with height

--- Tests/MunroQueryEngineTests/MunroQueryTests.swift
+++ Tests/MunroQueryEngineTests/MunroQueryTests.swift
@@ -3,19 +3,52 @@ import XCTest
 
 final class MunroQueryTests: XCTestCase {
     
-    func testMunroQueryHeightRange() {
-        let expectedHeightRange: Range<Float>
+    func testHeightRange() {
+        let expectedHeightRange: ClosedRange<Float> = Float.random(in: 0..<100)...Float.random(in: 100..<200)
         
-        let munroQuery = MunroQuery(heightRange: Range<Float>)
+        let munroQuery = try? MunroQuery(minimumHeightMeters: expectedHeightRange.lowerBound, maximumHeightMeters: expectedHeightRange.upperBound)
         
-        XCTAssertEqual(munroQuery.heightRange, expectedHeightRange)
+        
+        guard let realQuery = munroQuery else {
+            return XCTFail("Could not initialise query")
+        }
+        XCTAssertEqual(realQuery.heightRange, expectedHeightRange)
+    }
+    
+    func testInvalidHeightRange() {
+        let expectedMinimumHeight: Float = Float.random(in: 100..<200)
+        let expectedMaximumHeight: Float = Float.random(in: 0..<100)
+        
+        let munroQuery = try? MunroQuery(minimumHeightMeters: expectedMinimumHeight, maximumHeightMeters: expectedMaximumHeight)
+        
+        XCTAssertNil(munroQuery)
     }
 
     static var allTests = [
-        ("testMunroQueryHeightRange", testMunroQueryHeightRange),
+        ("testHeightRange", testHeightRange),
+        ("testInvalidHeightRange", testInvalidHeightRange)
     ]
 }
 
 struct MunroQuery {
+    let heightRange: ClosedRange<Float>?
     
+    init(minimumHeightMeters: Float?, maximumHeightMeters: Float?) throws {
+        switch (minimumHeightMeters, maximumHeightMeters) {
+        case let (.some(lowerHeight), .some(upperHeight)) where lowerHeight <= upperHeight:
+            heightRange = lowerHeight...upperHeight
+        case (.some(_), .some(_)):
+            throw MunroQueryError.badHeightRange
+        case let (nil, .some(upperHeight)):
+            heightRange = Float.leastNormalMagnitude...upperHeight
+        case let (.some(lowerHeight), nil):
+            heightRange = lowerHeight...Float.greatestFiniteMagnitude
+        case (nil, nil):
+            heightRange = nil
+        }
+    }
+}
+
+enum MunroQueryError: Error {
+    case badHeightRange
 }
