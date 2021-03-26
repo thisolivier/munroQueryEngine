ffcfdf3bcfeef46da16dece28e7a483f44be9bf0
Olivier Butler <olivier.butler@gmail.com>
2021-03-26 19:34:32 +0000

    Tidying up, going on break

---
+++ Tests/MunroQueryEngineTests/MunroQueryExecutorTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import MunroQueryEngine
+
+final class MunroQueryExecutorTests: XCTestCase {
+    
+//    static var allTests = [
+//        ("exampleTest", exampleTest),
+//    ]
+    
+    func testCanGetAllMunrosBack() {
+        
+    }
+    
+    // Probably check we're never getting munros without a type back
+}
